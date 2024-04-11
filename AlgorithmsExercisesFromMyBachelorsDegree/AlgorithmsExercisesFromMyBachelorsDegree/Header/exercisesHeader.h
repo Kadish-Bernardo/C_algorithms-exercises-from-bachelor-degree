c0a8814de6fe52daa8413f71921a00b0194ea4ce
Kadish Bernardo Ribeiro da Silva <kadish@unisantos.br>
2024-04-11 09:49:13 -0300

    Updating the project

---
+++ AlgorithmsExercisesFromMyBachelorsDegree/AlgorithmsExercisesFromMyBachelorsDegree/Header/exercisesHeader.h
@@ -0,0 +1,56 @@
+//
+//  exercisesHeader.h
+//  AlgorithmsExercisesFromMyBachelorsDegree
+//
+//  Created by Kadish Bernardo Ribeiro da Silva on 11/04/24.
+//
+
+#ifndef Header_h
+#define Header_h
+
+//  Library Declaration
+#include <stdio.h> //Standard Input-Output = stdio, Used for input and output operations, such as reading data from the keyboard and displaying data on the screen
+#include <stdlib.h> //Standard Library Header = stdlib, Used for memory manipulation, numerical conversions, pseudo-random number generation and other essential operations.
+#include <string.h> //Used for string manipulation, including copying, concatenation, comparison and search
+#include <stdbool.h> //Used to provide support for boolean data types and true and false constants
+
+
+//  Function Declaration in C
+//  return name(parameter;
+
+//  Example
+//  float dataStructure(void);
+//return = float
+//name = dataStructure
+//parameter = void
+
+//  1° Exercise
+char* CanGetDriversLicense(void);
+
+// 2° Exercise
+char* IsItAnEvenOrOddNumber(void);
+
+// 3° Exercise
+float TheGreatest(void);
+
+// 4° Exercise
+float TheMeadianValue(void);
+
+// 5° Exercise
+float IsItNegativeOrPositive(void);
+
+// 6° Exercise
+char* WhichDayIsit(void);
+
+// 7° Exercise
+char* WhichMonthisIt(void);
+
+// 8° Exercise
+float HowMuchIsTheEggs(void);
+
+// 9° Exercise
+char* HelloWorldLanguages(void);
+
+
+
+#endif /* Header_h */
---
+++ AlgorithmsExercisesFromMyBachelorsDegree/AlgorithmsExercisesFromMyBachelorsDegree/main.c
@@ -0,0 +1,49 @@
+//
+//  main.c
+//  AlgorithmsExercisesFromMyBachelorsDegree
+//
+//  Created by Kadish Bernardo Ribeiro da Silva on 11/04/24.
+//
+//  Library Declaration
+#include <stdio.h> // Standard Input-Output = stdio, Used for input and output operations, such as reading data from the keyboard and displaying data on the screen
+#include <stdlib.h> // Standard Library Header = stdlib, Used for memory manipulation, numerical conversions, pseudo-random number generation and other essential operations.
+#include <string.h> // Used for string manipulation, including copying, concatenation, comparison and search
+#include <stdbool.h> // Used to provide support for boolean data types and true and false constants
+
+
+//  Header Declaration
+#include "/Users/kadishsilva/Library/Mobile Documents/com~apple~CloudDocs/MyProfessionalCarrier/MyProjects/GitHub/C_algorithms-exercises-from-bachelor-degree/AlgorithmsExercisesFromMyBachelorsDegree/AlgorithmsExercisesFromMyBachelorsDegree/Header/exercisesHeader.h" //path
+
+int main(void) {
+    // 1° Exercise
+    /*char* driversLicenseAnswer = CanGetDriversLicense(); // Calling the function and storing the result in a variable
+     printf("%s", driversLicenseAnswer); // Here, we are printing the result on the screen using printf(), "%s" is a format specifier that indicates that we are printing a string, "DriversLicenseAnswer" is the string returned by the function
+     free(driversLicenseAnswer);  // Used to free up dynamically allocated memory, It is important to call free to avoid memory leaks in programs that dynamically allocate memory */
+    
+    // 2° Exercise
+    /*printf("%s", IsItAnEvenOrOddNumber()); // Calling the function and printing the result on the screen using printf(), "%s" is a format specifier that indicates that we are printing a string, "DriversLicenseAnswer" is the string returned by the function */
+    
+    
+    // 3° Exercise
+    /*printf("The greatest is %.2f \n", TheGreatest()); // Calling the fuction and priting the result on the screen using printf() */
+    
+    // 4° Exercise
+    /*printf("The median value is %.2f \n", TheMeadianValue()); // Calling the fuction and priting the result on the screen using printf() */
+    
+    // 5° Exercise
+    /*printf("%.2f \n", IsItNegativeOrPositive()); // Calling the fuction and priting the result on the screen using printf() */
+    
+    // 6° Exercise
+    /*printf("%s", WhichDayIsit()); // Calling the fuction and priting the result on the screen using printf(), "%s" is a format specifier that indicates that we are printing a string */
+    
+    // 7° Exercise
+    /*printf("%s", WhichMonthisIt()); // Calling the fuction and priting the result on the screen using printf(), "%s" is a format specifier that indicates that we are printing a string */
+    
+    // 8° Exercise
+    printf("The eggs will cost you R$ %.2f\n", HowMuchIsTheEggs()); // Calling the fuction and priting the result on the screen using printf()
+    
+    // 9° Exercise
+    
+    
+    return 0;
+}
